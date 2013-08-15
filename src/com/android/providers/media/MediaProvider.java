fbd078edcd788d876b9480fe8a690ac6195832dd
Maunik Shah <mshah@codeaurora.org>
2013-08-15 17:09:43 +0530

    Screenshots info is not updated when device is plugged in MTP mode
    When device is connected as MTP mode and user opens folder
    /sdcard/Pictures/Screenshots/ in windows system and captures new
    screenshot, image information is always shown as 0 KB
    
    Issue: https://code.google.com/p/android/issues/detail?id=56204
    
    OPO-216
    
    Change-Id: I0e633efc7a5b213e519d4de6b137992c5bfe8738
    (cherry picked from commit 5ad1eeb2968a7f3438f782df457558169007ed16)

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -2844,6 +2844,19 @@ public class MediaProvider extends ContentProvider {
         }
     }
 
+    private void sendObjectUpdated(long objectHandle) {
+        synchronized (mMtpServiceConnection) {
+            if (mMtpService != null) {
+                try {
+                    mMtpService.sendObjectUpdated((int)objectHandle);
+                } catch (RemoteException e) {
+                    Log.e(TAG, "RemoteException in sendObjectUpdated", e);
+                    mMtpService = null;
+                }
+            }
+        }
+    }
+
     @Override
     public int bulkInsert(Uri uri, ContentValues values[]) {
         int match = URI_MATCHER.match(uri);
@@ -4378,6 +4391,11 @@ public class MediaProvider extends ContentProvider {
                                         + count + " match = " + match);
                             }
                         }
+                        if (count > 0) {
+                            helper.mNumQueries++;
+                            notifyMtpUpdated(sGetTableAndWhereParam.table, db,
+                                    sGetTableAndWhereParam.where, whereArgs);
+                        }
                     }
                     break;
                 case IMAGES_MEDIA:
@@ -4422,6 +4440,11 @@ public class MediaProvider extends ContentProvider {
                                 }
                             }
                         }
+                        if (count > 0) {
+                            helper.mNumQueries++;
+                            notifyMtpUpdated(sGetTableAndWhereParam.table, db,
+                                    sGetTableAndWhereParam.where, whereArgs);
+                        }
                     }
                     break;
 
@@ -4444,6 +4467,11 @@ public class MediaProvider extends ContentProvider {
                     helper.mNumUpdates++;
                     count = db.update(sGetTableAndWhereParam.table, initialValues,
                         sGetTableAndWhereParam.where, whereArgs);
+                    if (count > 0) {
+                        helper.mNumQueries++;
+                        notifyMtpUpdated(sGetTableAndWhereParam.table, db,
+                                sGetTableAndWhereParam.where, whereArgs);
+                    }
                     break;
             }
         }
@@ -4453,6 +4481,20 @@ public class MediaProvider extends ContentProvider {
             getContext().getContentResolver().notifyChange(uri, null);
         }
         return count;
+    }
+
+    private void notifyMtpUpdated(String table, SQLiteDatabase db,
+            String userWhere, String[] whereArgs) {
+        Cursor c = db.query(table, ID_PROJECTION, userWhere, whereArgs, null, null, null);
+        if (c != null) {
+            try {
+                while (c.moveToNext()) {
+                    sendObjectUpdated(c.getLong(0));
+                }
+            } finally {
+                c.close();
+            }
+        }
     }
 
     private int movePlaylistEntry(DatabaseHelper helper, SQLiteDatabase db,
