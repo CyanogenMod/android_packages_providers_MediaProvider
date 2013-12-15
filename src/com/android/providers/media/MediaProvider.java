1e383b66424aec86dcd7f503d57f6fded3bf5624
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-12-15 09:25:57 +0700

    MediaProvider: allow write access to external SD
    Without this, Gallery cannot delete photos on external SD.
    
    Change-Id: Ifbebcdb167cca92a5a05158fa4c8b81512b77ba6

--- src/com/android/providers/media/MediaProvider.java
+++ src/com/android/providers/media/MediaProvider.java
@@ -4629,7 +4629,8 @@ public class MediaProvider extends ContentProvider {
                 (c.checkCallingOrSelfUriPermission(uri, Intent.FLAG_GRANT_READ_URI_PERMISSION)
                 == PackageManager.PERMISSION_GRANTED);
 
-        if (path.startsWith(sExternalPath) || path.startsWith(sLegacyPath)) {
+        if (path.startsWith(sExternalPath) || path.startsWith(sLegacyPath)
+                || isSecondaryExternalPath(path)) {
             if (!readGranted) {
                 c.enforceCallingOrSelfPermission(
                         READ_EXTERNAL_STORAGE, "External path: " + path);
@@ -4651,12 +4652,6 @@ public class MediaProvider extends ContentProvider {
         } else if (isWrite) {
             // don't write to non-cache, non-sdcard files.
             throw new FileNotFoundException("Can't access " + file);
-        } else if (isSecondaryExternalPath(path)) {
-            // read access is OK with the appropriate permission
-            if (!readGranted) {
-                c.enforceCallingOrSelfPermission(
-                        READ_EXTERNAL_STORAGE, "External path: " + path);
-            }
         } else {
             checkWorldReadAccess(path);
         }
