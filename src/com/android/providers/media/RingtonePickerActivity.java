29156f9d1812fda58366ef9ff0a3a70ad1e9d2d3
Jorge Ruesga <jorge@ruesga.com>
2014-03-22 12:05:11 +0100

    mediaprovider: Add theme support to Ringtone activity
    Usage:
    
    Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
    intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALARM);
    intent.putExtra(RingtoneManager.EXTRA_RINGTONE_DIALOG_THEME, R.style.CustomDialogTheme); <<< Add your custom dialog theme here
    startActivityForResult(intent, 0);
    
    Style Usage:
    
    Define an style that inherits from an android base theme
    
    <style name="CustomDialogThemeDark" parent="@*android:style/Theme.Holo.Dialog.Alert">
    
    or
    
    <style name="CustomDialogThemeLight" parent="@*android:style/Theme.Holo.Light.Dialog.Alert">
    
    or define your own dialog style
    
    Depends on http://review.cyanogenmod.org/#/c/61392
    
    Change-Id: I409eba2674f68386cdd20b74fc3ea42b8e81d111
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/providers/media/RingtonePickerActivity.java
+++ src/com/android/providers/media/RingtonePickerActivity.java
@@ -18,6 +18,9 @@ package com.android.providers.media;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.content.res.Resources.Theme;
 import android.database.Cursor;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -131,6 +134,21 @@ public final class RingtonePickerActivity extends AlertActivity implements
         mHandler = new Handler();
 
         Intent intent = getIntent();
+
+        // Set custom theme
+        int themeExtra = intent.getIntExtra(RingtoneManager.EXTRA_RINGTONE_DIALOG_THEME, 0);
+        if (themeExtra != 0) {
+            try {
+                Resources resources = getPackageManager().getResourcesForApplication(
+                        getCallingPackage());
+                Theme theme = resources.newTheme();
+                theme.applyStyle(themeExtra, true);
+                getTheme().setTo(theme);
+
+            } catch (NameNotFoundException e) {
+                // Resource not available. Fall-through default theme
+            }
+        }
 
         /*
          * Get whether to show the 'Default' item, and the URI to play when the
